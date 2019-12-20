b2f5309e106aab7f0b13bdd40af75a9781aa1a9c
Bertrand Delacretaz <bdelacretaz@apache.org>
2019-12-20 12:55:58 +0100

    SLING-8936 - refactor tests to get a reusable base class

--- src/test/java/org/apache/sling/servlets/resolver/internal/SlingServletResolverTestBase.java
+++ src/test/java/org/apache/sling/servlets/resolver/internal/SlingServletResolverTestBase.java
@@ -18,52 +18,26 @@
  */
 package org.apache.sling.servlets.resolver.internal;
 
-import static junit.framework.TestCase.assertTrue;
-import static org.junit.Assert.assertEquals;
-
 import java.lang.reflect.Field;
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-import javax.servlet.Servlet;
-import javax.servlet.http.HttpServlet;
-
-import org.apache.sling.api.SlingHttpServletRequest;
 import org.apache.sling.api.resource.LoginException;
-import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.api.resource.ResourceResolverFactory;
-import org.apache.sling.api.resource.ResourceUtil;
-import org.apache.sling.api.servlets.OptingServlet;
-import org.apache.sling.api.servlets.ServletResolverConstants;
-import org.apache.sling.commons.testing.osgi.MockServiceReference;
-import org.apache.sling.commons.testing.sling.MockResource;
 import org.apache.sling.commons.testing.sling.MockResourceResolver;
-import org.apache.sling.commons.testing.sling.MockSlingHttpServletRequest;
 import org.apache.sling.servlets.resolver.internal.resolution.ResolutionCache;
-import org.apache.sling.servlets.resolver.internal.resource.MockServletResource;
 import org.junit.Before;
-import org.junit.Test;
 import org.mockito.Mockito;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.Constants;
 
-public class SlingServletResolverTest {
+public abstract class SlingServletResolverTestBase {
 
-    private Servlet servlet;
+    protected SlingServletResolver servletResolver;
 
-    private SlingServletResolver servletResolver;
-
-    public static final String SERVLET_PATH = "/mock";
-
-    public static final String SERVLET_NAME = "TestServlet";
-
-    private static final String SERVLET_EXTENSION = "html";
-
-    private MockResourceResolver mockResourceResolver;
+    protected MockResourceResolver mockResourceResolver;
 
     @Before public void setUp() throws Exception {
         final ResolverConfig config = Mockito.mock(ResolverConfig.class);
@@ -130,7 +104,6 @@ public class SlingServletResolverTest {
             }
         };
 
-        servlet = new MockSlingRequestHandlerServlet();
         servletResolver = new SlingServletResolver();
 
         Class<?> resolverClass = servletResolver.getClass();
@@ -151,72 +124,15 @@ public class SlingServletResolverTest {
         final BundleContext bundleContext = Mockito.mock(BundleContext.class);
         Mockito.when(bundle.getBundleContext()).thenReturn(bundleContext);
 
-        MockServiceReference serviceReference = new MockServiceReference(bundle);
-        serviceReference.setProperty(Constants.SERVICE_ID, 1L);
-        serviceReference.setProperty(ServletResolverConstants.SLING_SERVLET_NAME,
-            SERVLET_NAME);
-        serviceReference.setProperty(
-                ServletResolverConstants.SLING_SERVLET_PATHS, SERVLET_PATH);
-        serviceReference.setProperty(
-            ServletResolverConstants.SLING_SERVLET_EXTENSIONS,
-            SERVLET_EXTENSION);
-
+        defineTestServlets(bundle);
         servletResolver.activate(config);
 
-        String path = "/"
-            + MockSlingHttpServletRequest.RESOURCE_TYPE
-            + "/"
-            + ResourceUtil.getName(MockSlingHttpServletRequest.RESOURCE_TYPE)
-            + ".servlet";
-        MockServletResource res = new MockServletResource(mockResourceResolver,
-            servlet, path);
-        mockResourceResolver.addResource(res);
-
-        MockResource parent = new MockResource(mockResourceResolver,
-            ResourceUtil.getParent(res.getPath()), "nt:folder");
-        mockResourceResolver.addResource(parent);
-
-        List<Resource> childRes = new ArrayList<>();
-        childRes.add(res);
-        mockResourceResolver.addChildren(parent, childRes);
     }
+
+    protected abstract void defineTestServlets(Bundle bundle);
 
     protected String getRequestWorkspaceName() {
         return "fromRequest";
-    }
-
-    @Test public void testAcceptsRequest() {
-        MockSlingHttpServletRequest secureRequest = new MockSlingHttpServletRequest(
-            SERVLET_PATH, null, SERVLET_EXTENSION, null, null);
-        secureRequest.setResourceResolver(mockResourceResolver);
-        secureRequest.setSecure(true);
-        Servlet result = servletResolver.resolveServlet(secureRequest);
-        assertEquals("Did not resolve to correct servlet", servlet, result);
-    }
-
-    @Test public void testIgnoreRequest() {
-        MockSlingHttpServletRequest insecureRequest = new MockSlingHttpServletRequest(
-            SERVLET_PATH, null, SERVLET_EXTENSION, null, null);
-        insecureRequest.setResourceResolver(mockResourceResolver);
-        insecureRequest.setSecure(false);
-        Servlet result = servletResolver.resolveServlet(insecureRequest);
-        assertTrue("Did not ignore unwanted request",
-            result.getClass() != MockSlingRequestHandlerServlet.class);
-    }
-
-    /**
-     * This sample servlet will only handle secure requests.
-     *
-     * @see org.apache.sling.api.servlets.OptingServlet#accepts
-     */
-    @SuppressWarnings("serial")
-    private static class MockSlingRequestHandlerServlet extends HttpServlet
-            implements OptingServlet {
-
-        @Override
-        public boolean accepts(SlingHttpServletRequest request) {
-            return request.isSecure();
-        }
     }
 
 }
