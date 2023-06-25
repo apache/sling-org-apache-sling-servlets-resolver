a001e4fafd8cc216d3b376b57640e45b3a691cfe
Jörg Hoh <joerg@joerghoh.de>
2023-06-25 21:32:48 +0200

    fix imports

--- src/main/java/org/apache/sling/servlets/resolver/internal/helper/LocationCollector.java
+++ src/main/java/org/apache/sling/servlets/resolver/internal/helper/LocationCollector.java
@@ -25,7 +25,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
 
 import org.apache.commons.lang3.StringUtils;
