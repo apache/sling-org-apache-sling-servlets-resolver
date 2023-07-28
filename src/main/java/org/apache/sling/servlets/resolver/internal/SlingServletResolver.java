f52c6304491213b581ac6558b031d002b5007d79
Jörg Hoh <joerg@joerghoh.de>
2023-07-28 19:18:46 +0200

    correct invalidation calls (2)

--- src/main/java/org/apache/sling/servlets/resolver/internal/SlingServletResolver.java
+++ src/main/java/org/apache/sling/servlets/resolver/internal/SlingServletResolver.java
@@ -393,7 +393,7 @@ public class SlingServletResolver
             }
             scriptResolver = this.sharedScriptResolver.get();
         }
-        invalidateCache(this.sharedScriptResolver.get());
+        invalidateCache(scriptResolver);
         return scriptResolver;
     }
 
